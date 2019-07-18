ad3c7a135b80dabfd70d3cad7a3d084bf61dc3db
Nigel Rodrigues <nigel1@ualberta.ca>
2019-07-17 23:34:08 -0700

    employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :bigint           not null, primary key
+#  end_date   :integer
+#  name       :string           not null
+#  province   :string
+#  start_date :integer
+#  wage       :float            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Employee < ApplicationRecord
     
+    validates :name, :wage, presence: true
+
+    has_many :payslips
+
 end
