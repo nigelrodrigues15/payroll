e12df80495b3d962068c40e14e3c67ebf063f896
Nigel Rodrigues <nigel1@ualberta.ca>
2019-07-24 23:37:25 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :payslips
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
-    resource :session, only: [:create, :destroy, :show]
+    # resources :users, only: [:create]
+    # resource :session, only: [:create, :destroy, :show]
     resources :employees, only: [:create, :destroy, :index, :show, :update]
     resources :payslips, only: [:create, :destroy, :index, :show, :update]
   end
