da482c256438279571ab4c0e966c4d20fbc9627f
Nigel Rodrigues <nigel1@ualberta.ca>
2019-07-28 23:45:54 -0700

    model

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :bigint           not null, primary key
+#  end_date   :integer
+#  name       :string           not null
+#  province   :string
+#  start_date :integer
+#  wage       :float            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
