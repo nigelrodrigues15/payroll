c0a83c46184a56b410120e639d10f3e407bbcf7d
Nigel Rodrigues <nigel1@ualberta.ca>
2019-08-26 13:00:12 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Employee.destroy_all
+Employee.create({name: 'Nigel Rodrigues', wage: '20'})
+Employee.create({name: 'Titus Rodrigues', wage: '50'})
+Employee.create({name: 'Person McPerson', wage: '5.50'})
