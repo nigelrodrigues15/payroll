3e6580449c5bde1ada0223fbb275303e97b51da7
Nigel Rodrigues <nigel1@ualberta.ca>
2019-11-01 23:38:52 -0700

    landing

---
+++ src/components/landing/landing.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import AddIcon from '@material-ui/icons/Add';
+import { Link } from "react-router-dom";
+
+const landing = () => {
+    return (
+        <div className="landing">
+            <div className='landing-image'></div>
+            <div className="newData">
+                <Link className="Link" to="/dashboard">
+                    <Button id='UIbutton' variant="contained" color="primary" startIcon={<AddIcon />}>
+                        Create New Data
+                    </Button>
+                </Link>
+            </div>
+            <br /><br /><br /><br />
+            <div className="oldData">
+                <Link className="Link" to="/dashboard">
+                    <Button id='UIbutton' variant="contained" color="primary" startIcon={<CloudUploadIcon />}>
+                        Upload Existing Data
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default landing;
