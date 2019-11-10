fcc33bbf4bae2696dca4c544e7c57dff24969323
Nigel Rodrigues <nigel1@ualberta.ca>
2019-11-10 05:10:12 -0800

    rootreducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,18 @@ import { combineReducers } from 'redux';
 import employeeReducer from './employeeReducer';
 import monthsReducer from './monthsReducer';
 
-export default combineReducers({
+const appReducer = combineReducers({
     employees: employeeReducer,
     months: monthsReducer
-});
+  })
+  
+const rootReducer = (state, action) => {
+
+    if (action.type === 'SET_DATABASE') {
+        return action.database
+    }
+
+    return appReducer(state, action)
+  }
+
+export default rootReducer
