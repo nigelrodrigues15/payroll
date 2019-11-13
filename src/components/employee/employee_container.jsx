d806d04d0b4e7e6a7a1139064a84e134b755d6c0
Nigel Rodrigues <nigel1@ualberta.ca>
2019-11-13 01:53:35 -0800

    employee

--- src/components/employee/employee_container.jsx
+++ src/components/employee/employee_container.jsx
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { fetchEmployees, createEmployee, showEmployee, removeEmployee } from "../../actions/employee_actions";
+import { updateEmployee, createEmployee, showEmployee, removeEmployee } from "../../actions/employee_actions";
 import Employee from "./employee";
 
 const mapStateToProps = (state, ownprops) => {
@@ -8,13 +8,14 @@ const mapStateToProps = (state, ownprops) => {
   let employeeData = state.employees[ownprops.match.params.employeeID]
   return {
     employee: employeeData,
-    payslips: employeeData.payslips
+    payslips: employeeData.payslips,
+    employeeID: ownprops.match.params.employeeID
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchEmployees: () => dispatch(fetchEmployees()),
+    updateEmployee: (id, info) => dispatch(updateEmployee(id, info)),
     createEmployee: employeeInfo => dispatch(createEmployee(employeeInfo)),
     showEmployee: () => dispatch(showEmployee()),
     removeEmployee: employeeId => dispatch(removeEmployee(employeeId))
