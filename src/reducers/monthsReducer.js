07482064f7728e5b70b3faffdad7239dda8124d7
Nigel Rodrigues <nigel1@ualberta.ca>
2019-11-14 01:54:05 -0800

    motnh

--- src/reducers/monthsReducer.js
+++ src/reducers/monthsReducer.js
@@ -1,5 +1,5 @@
 import merge from "lodash/merge";
-import { FETCH_EMPLOYEES, CREATE_EMPLOYEE, SHOW_EMPLOYEE, REMOVE_EMPLOYEE } from "../actions/employee_actions";
+// import { UPDATE_EMPLOYEE, CREATE_EMPLOYEE, SHOW_EMPLOYEE, REMOVE_EMPLOYEE } from "../actions/employee_actions";
 
 
 const monthsReducer = (state = {}, action) => {
@@ -8,27 +8,21 @@ const monthsReducer = (state = {}, action) => {
   console.log(state);
   switch (action.type) {
 
-    case FETCH_EMPLOYEES:
+    case 'UPDATE_EMPLOYEE':
       console.log('fetching employees')
       console.log(newState)
       // merge(newState,{[action.company.id]: action.company});
 
-    return action.payload;
+      return state;
 
-    case CREATE_EMPLOYEE:
-    return [
-        ...state,
-        {
-            id: action.id,
-            info: action.employeeInfo
-        }
-    ]
+    case 'CREATE_EMPLOYEE':
+      return state
 
-    case SHOW_EMPLOYEE:
-    return action.payload;
+    case 'SHOW_EMPLOYEE':
+      return state;
 
-    case REMOVE_EMPLOYEE:
-    return action.payload;
+    case 'REMOVE_EMPLOYEE':
+      return state;
 
     default:
       return state;
