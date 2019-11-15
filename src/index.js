f5d1329087c64e9800592ad404e1d8453e3f5249
Nigel Rodrigues <nigel1@ualberta.ca>
2019-11-15 01:54:07 -0800

    index

--- src/index.js
+++ src/index.js
@@ -9,15 +9,39 @@ import * as serviceWorker from './serviceWorker';
 
 export const preloadedState = require('./database.json')
 // export const preloadedState = {}
+const persistedState = loadFromLocalStorage()
+
+function save2localStorage(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (error) {
+        console.log(error);
+    }
+}
+function loadFromLocalStorage(state) {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) return preloadedState;
+        return JSON.parse(serializedState);
+    } catch (error) {
+        console.log(error);
+        return preloadedState;
+    }
+}
+
+let store = configureStore(persistedState)
 
 ReactDOM.render(
-    <Provider store={configureStore(preloadedState)}>
-         <HashRouter>
+    <Provider store={store}>
+        <HashRouter>
             <App />
         </HashRouter>
-    </Provider>, 
-    document.getElementById('root'));
+    </Provider>,
+    document.getElementById('root')
+);
 
+store.subscribe(() => save2localStorage(store.getState()))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
