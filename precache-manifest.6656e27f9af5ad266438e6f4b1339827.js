f0013ff91211549059a3126a7ad1ab79794491fb
Nigel Rodrigues <nigel1@ualberta.ca>
2019-12-23 23:28:11 -0800

    Updates

---
+++ precache-manifest.6656e27f9af5ad266438e6f4b1339827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b04ed2b5bf56f35da013a53125d5f50",
+    "url": "/Payrollz/index.html"
+  },
+  {
+    "revision": "451317245336f2bc9f7d",
+    "url": "/Payrollz/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "34018150c11ccce51748",
+    "url": "/Payrollz/static/js/2.b6c55dd8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Payrollz/static/js/2.b6c55dd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "451317245336f2bc9f7d",
+    "url": "/Payrollz/static/js/main.bcf579f0.chunk.js"
+  },
+  {
+    "revision": "6e639894a02fff42f772",
+    "url": "/Payrollz/static/js/runtime-main.757a2628.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Payrollz/static/media/logo.5d5d9eef.svg"
+  }
+]);
