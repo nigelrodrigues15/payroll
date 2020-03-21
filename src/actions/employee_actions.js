33d23d4a3246a1b919ada47d4aca843a3e8f1515
Nigel Rodrigues <nigel1@ualberta.ca>
2020-03-21 00:10:02 -0700

    actions

--- src/actions/employee_actions.js
+++ src/actions/employee_actions.js
@@ -1,20 +1,20 @@
-export const FETCH_EMPLOYEES = 'FETCH_EMPLOYEES'
-export const fetchEmployees = payload => {
-    return {
-        type: FETCH_EMPLOYEES,
-        payload
-    };
-};
+export const FETCH_EMPLOYEES = 'FETCH_EMPLOYEES';
+export const fetchEmployees = () => dispatch => dispatch({
+    type: FETCH_EMPLOYEES
+});
 
-export const CREATE_EMPLOYEE = 'CREATE_EMPLOYEE'
+
+export const CREATE_EMPLOYEE = 'CREATE_EMPLOYEE';
+export const nextEmployeeId = 0;
 export const createEmployee = (employeeInfo) => dispatch => {
     dispatch({
         type: CREATE_EMPLOYEE,
+        id: nextEmployeeId++,
         employeeInfo
     })
 };
 
-export const SHOW_EMPLOYEE = 'SHOW_EMPLOYEE'
+export const SHOW_EMPLOYEE = 'SHOW_EMPLOYEE';
 export const showEmployee = employeeId => {
     return {
         type: SHOW_EMPLOYEE,
@@ -22,7 +22,7 @@ export const showEmployee = employeeId => {
     };
 };
 
-export const REMOVE_EMPLOYEE = 'REMOVE_EMPLOYEE'
+export const REMOVE_EMPLOYEE = 'REMOVE_EMPLOYEE';
 export const removeEmployee = (employeeId) => dispatch => {
     dispatch({
      type: REMOVE_EMPLOYEE,
